24148e5915908fc3565914378a4841a0434fd08d
Artur Zubarev <arturzubarevvrn@gmail.com>
2023-01-08 12:26:27 +0300

    initial commit

---
+++ src/test/java/org/zubarev/java_vue/JavaVueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zubarev.java_vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
